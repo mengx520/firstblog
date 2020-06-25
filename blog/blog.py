8ed196c1871d605ba7835ffae0b42a376fc8ce6f
mengx520 <mengx520@gmail.com>
2020-06-24 17:09:24 -0700

    Convert tabs to spaces

--- blog/blog.py
+++ blog/blog.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-	return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-	return 'About'
+    return 'About'
 
 @app.route('/recipe')
 def recipe():
-	return 'Recipe'
+    return 'Recipe'
 
 @app.route('/hello/<name>')
 def hello(name):
-	return render_template('hello.html', username=name)
+    return render_template('hello.html', username=name)
