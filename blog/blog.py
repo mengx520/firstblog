d08d4d607167d5f873ee059b606b200f69ecb971
mengx520 <mengx520@gmail.com>
2020-06-24 17:04:11 -0700

    Add html templates

--- blog/blog.py
+++ blog/blog.py
@@ -1,9 +1,12 @@
-from flask import Flask 
+from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-	return 'Home Page'
+	return render_template('home.html')
 
 @app.route('/about')
 def about():
@@ -15,4 +18,4 @@ def recipe():
 
 @app.route('/hello/<name>')
 def hello(name):
-	return 'Hello ' + name
+	return render_template('hello.html', username=name)
