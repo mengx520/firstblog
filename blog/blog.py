8995d1cebc3a1c30335fdfd9807790c77f7f5f3a
mengx520 <mengx520@gmail.com>
2020-07-17 17:58:50 -0700

    Add base.html and simplify templates

--- blog/blog.py
+++ blog/blog.py
@@ -6,27 +6,27 @@ app = Flask(__name__)
 body = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 @app.route('/')
 def home_page():
-    return render_template('home.html', title='Home', content=body)
+    return render_template('base.html', title='Home', content=body)
 
 @app.route('/about')
 def about():
-    return render_template('home.html', title='About', content=body)
+    return render_template('base.html', title='About', content=body)
 
 @app.route('/learn')
 def recipe():
-    return render_template('home.html', title='Learn', content=body)
+    return render_template('base.html', title='Learn', content=body)
 
 @app.route('/food')
 def food():
-    return render_template('home.html', title='Food', content=body)
+    return render_template('base.html', title='Food', content=body)
 
 @app.route('/photo')
 def photo():
-    return render_template('home.html', title='Photo', content=body)
+    return render_template('base.html', title='Photo', content=body)
 
 @app.route('/art')
 def art():
-    return render_template('home.html', title='Art', content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+    return render_template('home.html', title='Art', content=body)
 
 @app.route('/hello/<name>')
 def hello(name):
